09d7a115b06effaccf1e378d147292f4225f39ee
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-10-10 01:00:51 +0500

    fix lint

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,6 @@ fn index() -> &'static str {
 }
 
 #[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+async fn main() {
+    let _ = rocket::build().mount("/", routes![index]).launch().await;
 }
